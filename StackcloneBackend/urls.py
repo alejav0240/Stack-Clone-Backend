70580c2184188eaed1903f3a6c2fa9ae8c7a1ecc
Alejandro <cra2028252@est.univalle.edu>
2025-04-26 01:48:46 -0400

    feat: config

--- StackcloneBackend/urls.py
+++ StackcloneBackend/urls.py
@@ -1,22 +1,21 @@
-"""
-URL configuration for StackcloneBackend project.
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# from django.contrib import admin
-from django.urls import path
+router = routers.DefaultRouter()
+# Registra aquí los ViewSets de diferentes aplicaciones si es un router global
+# router.register(r'users', users_views.UserViewSet, basename='user')
+# router.register(r'questions', questions_views.QuestionViewSet, basename='question')
+# ...
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
+    path('api/', include([
+        path('', include(router.urls)),
+        path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+        # path('users/', include('users.urls')),
+        # path('questions/', include('questions.urls')),
+        # path('articles/', include('articles.urls')),
+        # path('achievements/', include('achievements.urls')),
+    ])),
 ]
